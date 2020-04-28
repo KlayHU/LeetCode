f9775d375a63109d0b8ccce16b2925f6f6f7e3fd
KlayHu <852827312@qq.com>
2020-04-28 23:05:01 +0800

    leetcode3.30~4.30每日一题

--- src/Leetcode/everydayQuest/search.java
+++ src/Leetcode/everydayQuest/search.java
@@ -1,9 +1,44 @@
 package Leetcode.everydayQuest;
 
 /**
- * @description:
+ * @description: 每日一题：旋转数组中的查找，要求：时间复杂度O(log(n)) 【难度】：中等
+ * 用二分查找，暴力超时了
  * @author: KlayHu
  * @create: 2020/4/27 17:44
  **/
 public class search {
+    public int search1(int[] nums, int target) {
+
+        if (nums.length == 0 || nums == null) {
+            return -1;
+        }
+        //下标
+        int start = 0;
+        int end = nums.length - 1;
+        int mid;
+        while (start <= end) {
+            mid = start + (end + start) / 2;
+            if (target == nums[mid]) {
+                return mid;
+            }
+            if (nums[start] <= nums[mid]) {
+                //target落在前半段
+                if (target >= nums[start] && target < nums[mid]) {
+                    end = mid - 1;//新end，分割
+                } //在后半段去找
+                else {
+                    start = mid + 1;
+                }
+            } else {
+                //target落在后半段
+                if (target <= nums[end] && target > nums[mid]) {
+                    start = mid + 1;
+                } //在前半段再去找
+                else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
 }
