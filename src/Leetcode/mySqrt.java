083986c9177d88dffb24f00592f7b8bf6170648b
KlayHu <852827312@qq.com>
2020-07-29 15:05:50 +0800

    leetcode 067 二进制求和 [难度：简单] 069 x的平方根 [难度：简单]提交题记

---
+++ src/Leetcode/mySqrt.java
@@ -0,0 +1,28 @@
+package Leetcode;
+
+/**
+ * @description: 69.x的平方根   【难度：简单】
+ * 思路：二分查找，对于给定的x，必有0<=y^2<=x，由此可以推出查找上下界
+ * @author: KlayHu
+ * @create: 2020/7/29 14:18
+ **/
+public class mySqrt {
+    public int mySqrt1(int x){
+        int down =0,up=x;       //上下界
+        int result=0;
+        while (up - down>=0) {
+            int mid = down+(up-down)/2;     //中间区间值
+            if ((long)mid * mid <= x) {
+                result=mid;
+                down = mid+1;
+            }else {
+                up = mid-1;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        new mySqrt().mySqrt1(8);
+    }
+}
